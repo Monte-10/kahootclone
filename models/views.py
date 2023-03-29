ed1ad8cad143ba8a1f2d49e2cf420255b13a5537
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-03-29 17:22:14 +0200

    Cambios para singup

--- models/views.py
+++ models/views.py
@@ -4,12 +4,30 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
 
 def home(request):
     return render(request, 'home.html')
 
 def singup(request):
-    return render(request, 'sing_up.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+        
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'singup.html', {'form': form})
+            
