b7aa5bce82ed6d2d16a65fd01a64474bec13d528
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-04-12 23:31:05 +0200

    Sino no funciona creo

--- kahootclone/settings.py
+++ kahootclone/settings.py
@@ -165,9 +165,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.'
-    'storage.'
-    'CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
