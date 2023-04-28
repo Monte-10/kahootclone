b3835c82d6c2155b7f207e19be5d0e7b1459ce53
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-04-28 21:53:24 +0200

    Que si

--- kahootclone/settings.py
+++ kahootclone/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+AUTORES: Alejandro Monterrubio, Juan Francisco Flores, Pablo Hernaez.
 """
 
 import dj_database_url
